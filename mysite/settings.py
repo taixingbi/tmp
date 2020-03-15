f57257cb6e2dc776eadd6c008f0ef7aaff612bcb
taixingbi <bitaihang@gmail.com>
2020-03-15 10:44:02 -0400

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -192,8 +192,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'bitaihang@gmail.com'
-EMAIL_HOST_PASSWORD = 13050677228
+EMAIL_HOST_USER = 
+EMAIL_HOST_PASSWORD = 
 
 # aws access s3
 AWS_ACCESS_KEY_ID=env('AWS_ACCESS_KEY_ID')
